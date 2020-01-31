5194705529e25da421e7b99049fd06a93377371e
Kyle Hooks <dmuntrasi@gmail.com>
2020-01-31 02:38:37 -0600

    Added template unitypackage assets

---
+++ TemplateProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	public SceneTransitionScript transition;
+	public GameObject main;
+	public GameObject settings;
+	public Slider music;
+	public Slider sfx;
+
+	public void ToggleMainMenu()
+	{
+		main.SetActive(true);
+		settings.SetActive(false);
+	}
+
+	public void ToggleSettingsMenu()
+	{
+		settings.SetActive(true);
+		main.SetActive(false);
+	}
+
+	public void StartGame()
+	{
+		transition.GoFade();
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	public void SetSliderValues(SetAudioLevels audioScript)
+	{
+		music.value = audioScript.GetMusicLevel();
+		sfx.value = audioScript.GetSfxLevel();
+	}
+
+}
